c40a5ae74e52896d8d68265857067da49f1f905c
atanasster <atanasster@gmail.com>
2020-12-05 12:16:27 -0500

    upgrade packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import NextApp from 'next/app';
-import { CacheProvider } from '@emotion/core';
 import '../src/tailwind.output.css';
 
-// Use only { cache } from 'emotion'. Don't use { css }.
-import { cache } from 'emotion';
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <CacheProvider value={cache}>
-        <Component {...pageProps} />
-      </CacheProvider>
+      <Component {...pageProps} />
     );
   }
 }
