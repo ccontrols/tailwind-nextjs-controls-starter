bfdf5f09f1ad513e70f221edaea99b638037bfb8
atanasster <atanasster@gmail.com>
2020-12-24 01:17:33 -0500

    upgrade packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,18 @@
+const { defaultBuildConfig } = require('@component-controls/core');
+
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   siteUrl: 'https://tailwind-gatsby-controls-starter.netlify.app',
   pages: {
     story: {
-      tabs: [{ route: 'page' }, { route: 'test' }],
+      tabs: [
+        ...defaultBuildConfig.pages.story.tabs,
+        {
+          route: 'test',
+          title: 'Testing',
+          template: '@component-controls/pages/TestingPage',
+        },
+      ],
     },
   },
 };
